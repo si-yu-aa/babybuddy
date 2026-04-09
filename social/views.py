6877228029271f141101785c5416de30fcd6ed7e
siyu <siyu@bigai.ai>
2026-04-09 12:56:43 +0800

    feat: add feed template view

--- social/views.py
+++ social/views.py
@@ -1,2 +1,12 @@
 # -*- coding: utf-8 -*-
-# Placeholder views module - will be populated in Task 7
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+
+
+class FeedView(LoginRequiredMixin, TemplateView):
+    template_name = "social/feed.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["page_title"] = "Social Feed"
+        return context
